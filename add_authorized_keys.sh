9b0f41f279b2bcbb7f14aa9c9b1a8e320b883a79
bernhard <be.ka@aon.at>
2025-03-30 16:15:26 +0200

    done refactoring until hadoop installation

---
+++ add_authorized_keys.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+KEY_NAME='id_ed25519'
+USER='ansible'
+HOSTS=('vm-controller' 'vm-worker01' 'vm-worker02' 'vm-worker03')
+
+cd ~/.ssh
+ssh-keygen -t ed25519 -f ~/.ssh/${KEY_NAME}
+
+for HOST in "${HOSTS[@]}"
+do
+    ssh $USER@$HOST 'mkdir -p ~/.ssh'
+    scp ~/.ssh/${KEY_NAME}.pub $USER@$HOST:~/.ssh/${KEY_NAME}.pub
+    ssh $USER@$HOST "cat ~/.ssh/$KEY_NAME.pub >> ~/.ssh/authorized_keys"
+done
+
