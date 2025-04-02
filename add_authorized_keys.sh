8aa3e6af3cbf350ed8cde9c846dc87d991d21c65
bernhard <be.ka@aon.at>
2025-04-02 19:51:44 +0200

    kerberized cluster

--- add_authorized_keys.sh
+++ add_authorized_keys.sh
@@ -3,7 +3,6 @@
 KEY_NAME='id_ed25519'
 USER='ansible'
 HOSTS=('vm-controller' 'vm-worker01' 'vm-worker02' 'vm-worker03')
-HOSTS=('vm-worker02')
 
 cd ~/.ssh
 ssh-keygen -t ed25519 -f ~/.ssh/${KEY_NAME}
---
+++ keystore_trustsore_generator.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Configuration
+KEYSTORE_PASSWORD="bigdata"
+TRUSTSTORE_PASSWORD="bigdata"
+VALIDITY_DAYS=360
+KEY_SIZE=2048
+
+# List of hosts (modify as needed)
+HOSTS=('vm-controller' 'vm-worker01' 'vm-worker02' 'vm-worker03')
+
+# Output directories
+OUTPUT_DIR="./certs"
+mkdir -p "$OUTPUT_DIR"
+
+# Step 1: Generate keystores and certificates
+for HOST in "${HOSTS[@]}"; do
+    echo "Generating keystore for $HOST..."
+    
+    keytool -genkeypair -keyalg RSA \
+        -alias "$HOST" \
+        -keystore "$OUTPUT_DIR/$HOST-keystore.jks" \
+        -storepass "$KEYSTORE_PASSWORD" \
+        -validity "$VALIDITY_DAYS" \
+        -keysize "$KEY_SIZE" \
+        -dname "CN=$HOST, OU=Hadoop, O=kahovec, L=vienna, ST=vienna, C=austria" \
+        -noprompt
+    
+    echo "Exporting certificate for $HOST..."
+    keytool -export -alias "$HOST" \
+        -file "$OUTPUT_DIR/$HOST.crt" \
+        -keystore "$OUTPUT_DIR/$HOST-keystore.jks" \
+        -storepass "$KEYSTORE_PASSWORD" \
+        -rfc
+done
+
+# Step 2: Create a truststore and import all certificates
+echo "Creating truststore..."
+TRUSTSTORE_FILE="$OUTPUT_DIR/truststore.jks"
+rm -f "$TRUSTSTORE_FILE"
+
+for HOST in "${HOSTS[@]}"; do
+    echo "Importing $HOST certificate into truststore..."
+    keytool -import -alias "$HOST" \
+        -file "$OUTPUT_DIR/$HOST.crt" \
+        -keystore "$TRUSTSTORE_FILE" \
+        -storepass "$TRUSTSTORE_PASSWORD" \
+        -noprompt
+done
+
+echo "Done!"
