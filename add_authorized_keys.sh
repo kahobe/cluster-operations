2f6832e4be48bae2d01e68c5fe15570a385acf18
Bernhard Kahovec <bernhard@kahovec.dev>
2025-04-23 12:20:18 +0200

    adding tez

--- add_authorized_keys.sh
+++ add_authorized_keys.sh
@@ -2,7 +2,7 @@
 
 KEY_NAME='id_ed25519'
 USER='ansible'
-HOSTS=('vm-controller' 'vm-worker01' 'vm-worker02' 'vm-worker03')
+HOSTS=('vm-controller' 'vm-worker01')
 
 cd ~/.ssh
 ssh-keygen -t ed25519 -f ~/.ssh/${KEY_NAME}
